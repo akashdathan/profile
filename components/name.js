3078382c1a10fae7d2504303dfc04214616062a8
akash_dathan <akashdathan@Akashs-MacBook-Pro.local>
2020-07-02 02:23:58 +0800

    feat: name format

--- components/name.js
+++ components/name.js
@@ -1,12 +1,10 @@
 
 export default function Button() {
   return (
-    <div>
-      <p>
-       <div>{'< '}</div>
-       akash_dathan
-       <div>{' >'}</div>
-      </p>
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <div style={{ margin: '16px', marginRight: '5px'}}>{'<'}</div>
+      <p>akash_dathan</p>
+      <div style={{ margin: '16px', marginLeft: '5px'}}>{'>'}</div>
     </div>
   )
 }
