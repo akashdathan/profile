0b8bb1f4e4b09ab7a273c06bc17e3074d5ba4048
akash_dathan <akashdathan@Akashs-MacBook-Pro.local>
2020-07-02 00:23:54 +0800

    feat: index updated

---
+++ components/name.js
@@ -0,0 +1,12 @@
+
+export default function Button() {
+  return (
+    <div>
+      <p>
+       <div>{'< '}</div>
+       akash_dathan
+       <div>{' >'}</div>
+      </p>
+    </div>
+  )
+}
---
+++ pages/home.js
@@ -0,0 +1,11 @@
+import Name from '../components/name';
+
+function HomePage() {
+  return (
+    <div>
+      <Name/>
+    </div>
+  )
+}
+
+export default HomePage;
