50bca02509948d6e4dad7d7db7b669db360cb97f
akash_dathan <akashdathan@Akashs-MacBook-Pro.local>
2020-07-02 12:34:31 +0800

    feat: logo

--- components/name.js
+++ components/name.js
@@ -2,9 +2,10 @@
 export default function Button() {
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <div style={{ margin: '16px', marginRight: '5px'}}>{'<'}</div>
-      <p>akash_dathan</p>
-      <div style={{ margin: '16px', marginLeft: '5px'}}>{'>'}</div>
+      <div style={{ margin: '16px', marginRight: '5px', color: '#39ff14' }}>{'<'}</div>
+      <p style={{ color: '#39ff14' }}>akash_dathan</p>
+      <div style={{ margin: '16px', marginLeft: '5px', color: '#39ff14' }}>{'> $'}</div>
+      <div style={{ margin: '16px', marginLeft: '5px' }} class="blink" />
     </div>
   )
 }
