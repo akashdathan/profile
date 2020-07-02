03eb1f85c83bf9256ef4d899d7bab089545c9b59
akash_dathan <akashdathan@Akashs-MacBook-Pro.local>
2020-07-02 12:59:23 +0800

    feat: hello worls

--- components/name.js
+++ components/name.js
@@ -5,7 +5,8 @@ export default function Button() {
       <div style={{ margin: '16px', marginRight: '5px', color: '#39ff14' }}>{'<'}</div>
       <p style={{ color: '#39ff14' }}>akash_dathan</p>
       <div style={{ margin: '16px', marginLeft: '5px', color: '#39ff14' }}>{'> $'}</div>
-      <div style={{ margin: '16px', marginLeft: '5px' }} class="blink" />
+      <p style={{ marginLeft: '10px'}}>Hello World!</p>
+      <div style={{ margin: '12px', marginLeft: '0px' }} class="blink" />
     </div>
   )
 }
